224442ffba13e54c6abf95f74339eb60662665ae
AntohaFroll <antohafroll@gmail.com>
2023-02-12 16:16:51 +0300

    product page complete

--- pages/product_page.py
+++ pages/product_page.py
@@ -4,11 +4,8 @@ from .locators import ProductPageLocators
 
 class ProductPage(BasePage):
     def add_product_to_basket(self):
-        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
-        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
         button_add_to_basket = self.browser.find_element(*ProductPageLocators.BUTTON_ADD_TO_BASKET)
         button_add_to_basket.click()
-        return product_name, product_price
 
     def should_be_product_in_basket(self):
         assert self.is_element_present(*ProductPageLocators.ADDED_FORM_IN_BASKET), \
